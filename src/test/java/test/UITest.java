7323c8d06a43d51d66e425d2690fe8dd354c6a40
Егор Старшелюк <granegoro@gmail.com>
2022-12-20 18:05:28 +0300

    Update test

--- src/test/java/test/UITest.java
+++ src/test/java/test/UITest.java
@@ -25,13 +25,7 @@ public class UITest {
     @Test
     public void shouldLoginWithTestCredentials() {
         var loginPage = open(System.getProperty("sut.url"), LoginPage.class);
-        performLogin();
-
-
-
-
-
-
-
+        var mainPage = loginPage.performLogin();
+        
     }
 }
